6a710900a94939d1741a3b00311bc92e0a48e43f
Chris Claxton <c.d.claxton@gmail.com>
2024-01-29 08:11:24 +0000

    Correct tests following change to class

--- Python/DataScience/entity-extraction/likelihood/test_likelihood.py
+++ Python/DataScience/entity-extraction/likelihood/test_likelihood.py
@@ -1,21 +1,23 @@
-from .likelihood import likelihood
+from .likelihood import LikelihoodFunctionProbMissing
 
 
-def test_likelihood():
-    """Unit tests for likelihood()."""
+def test_likelihood_missing_prob():
+    """Unit tests for LikelihoodFunctionProbMissing class."""
 
     def is_close(x, y):
         return abs(x - y) < 1e-6
 
     p_m = 0.2
 
+    likelihood = LikelihoodFunctionProbMissing(p_m)
+
     # None missing
-    assert is_close(likelihood(["A"], ["A"], p_m), (1 - p_m))
-    assert is_close(likelihood(["A", "B"], ["A", "B"], p_m), (1 - p_m) ** 2)
+    assert is_close(likelihood.calc(["A"], ["A"]), (1 - p_m))
+    assert is_close(likelihood.calc(["A", "B"], ["A", "B"]), (1 - p_m) ** 2)
 
     # One missing
-    assert is_close(likelihood(["A", "B"], ["A"], p_m), (1 - p_m) * p_m)
-    assert is_close(likelihood(["A", "B", "C"], ["A", "C"], p_m), (1 - p_m) ** 2 * p_m)
+    assert is_close(likelihood.calc(["A"], ["A", "B"]), (1 - p_m) * p_m)
+    assert is_close(likelihood.calc(["A", "C"], ["A", "B", "C"]), (1 - p_m) ** 2 * p_m)
 
     # Two missing
-    assert is_close(likelihood(["A", "B", "C"], ["A"], p_m), (1 - p_m) * p_m**2)
+    assert is_close(likelihood.calc(["A"], ["A", "B", "C"]), (1 - p_m) * p_m**2)

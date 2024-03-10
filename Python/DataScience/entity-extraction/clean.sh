181abca80a427dc0f1e2400734f6015917659a35
Chris Claxton <c.d.claxton@gmail.com>
2024-03-10 16:09:29 +0000

    Tidy up clean script

--- Python/DataScience/entity-extraction/clean.sh
+++ Python/DataScience/entity-extraction/clean.sh
@@ -1,4 +1,19 @@
 echo "Removing all data ..."
-rm -rf ./data/lmdb
-rm ./data/token-to-count.pickle
-rm ./data/sqlite.db
+
+lmdb_folder=./data/lmdb
+if [ -d ${lmdb_folder} ]; then
+    echo "Deleting LMDB database"
+    rm -rf ${lmdb_folder}
+fi
+
+token_count_file=./data/token-to-count.pickle
+if [ -f ${token_count_file} ]; then
+    echo "Deleting token-to-count Pickle file"
+    rm ${token_count_file}
+fi
+
+sqlite_file=./data/sqlite.db
+if [ -f ${sqlite_file} ]; then
+    echo "Deleting temporary Sqlite database"
+    rm ${sqlite_file}
+fi

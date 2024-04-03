2950e98a881b919eb9c0ad354a82e37c1740511f
Chris Claxton <c.d.claxton@gmail.com>
2024-04-03 20:49:14 +0100

    Add docstrings

--- Python/DataScience/entity-extraction/metrics/positions.h
+++ Python/DataScience/entity-extraction/metrics/positions.h
@@ -29,29 +29,33 @@ SparsePositionResults positions(char *str,
                                 uint32_t max_entity_id,
                                 uint8_t min_count);
 
-// // Display a single position result on stdout.
-// void display_position(EntityPositions result);
-
-// // Display the position results on stdout.
-// void display_positions(SparsePositionResults results);
-
 // Counts the occurrences of each entity in the string.
 uint8_t *count_occurrences(char *str,
                            uint32_t max_entity_id);
 
+// Initialise the dense entity positions results given the `counts` of each
+// entity ID. An entity is only processed if it occurs at least `min_count`
+// times.
 DenseEntityPositions *initialise_position_results(uint8_t *counts,
                                                   uint32_t max_entity_id,
                                                   uint8_t min_count);
 
+// Walk through the string `str` and record the positions of the entities
+// that occur with a minimum count of `min_count`.
 DenseEntityPositions *populate_position_results(char *str,
                                                 uint32_t max_entity_id,
                                                 uint8_t min_count,
                                                 uint8_t *counts);
 
+// Convert the dense entity positions to a sparse representation, thus removing
+// entities that have an insufficient count.
 SparsePositionResults compact(DenseEntityPositions *results,
                               uint32_t max_entity_id);
 
+// Free the dynamically allocated memory for a DenseEntityPositions struct with
+// a maximum entity ID of `max_entity_id`.
 void free_dense_entity_positions(DenseEntityPositions *positions,
                                  uint32_t max_entity_id);
 
+// Free the dynamically allocated memory for a SparsePositionResults struct.
 void free_sparse_position_results(SparsePositionResults *sparse_results);

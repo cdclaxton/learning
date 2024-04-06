283faa1963037e873275b60666d3aa1a13b1409a
Chris Claxton <c.d.claxton@gmail.com>
2024-04-06 13:06:28 +0100

    Handle arbitrary length text inputs

--- Python/DataScience/entity-extraction/entity/matcher_add_remove.py
+++ Python/DataScience/entity-extraction/entity/matcher_add_remove.py
@@ -1,13 +1,37 @@
-from typing import List
+from typing import List, Tuple
 from loguru import logger
 from domain import Tokens, assert_probability_valid, assert_token_valid
 from entity.matcher import EntityMatcher, ProbabilisticMatch
 from likelihood.likelihood_add_remove import LikelihoodAddRemoveFn
 from lookup.lookup import Lookup
 
-# from metrics_compiled_c_arrays import calc_metrics
 from positions_compiled_c import calc_positions
 from adds_removes import adds_removes_from_positions
+
+
+def subdivide_text(
+    num_tokens: int, max_segment_length: int, max_window: int
+) -> List[Tuple[int, int]]:
+    """Sub-divide the text into segments for processing."""
+
+    assert type(num_tokens) == int and num_tokens > 0
+    assert type(max_segment_length) == int and max_segment_length >= max_window
+    assert type(max_window) == int and max_window > 0
+
+    # All of the tokens fit within a single segment
+    if num_tokens <= max_segment_length:
+        return [(0, num_tokens)]
+
+    # First segment
+    result = [(0, max_segment_length)]
+
+    # Remaining segments
+    while result[-1][1] < num_tokens:
+        start = result[-1][1] - max_window + 1
+        end = min(start + max_segment_length, num_tokens)
+        result.append((start, end))
+
+    return result
 
 
 class EntityMatcherAddRemove(EntityMatcher):
@@ -68,44 +92,60 @@ class EntityMatcherAddRemove(EntityMatcher):
             f"Minimum number of times the entity must appear for evaluation: {min_count}"
         )
 
-        # Call compiled C code to find the entity positions
-        s = "|".join(self._entity_ids_str)
-        position_results = calc_positions(s.encode(), self._max_entity_id, min_count)
+        # Divide the text into segments that can be processed by the compiled C
+        # code. The code uses 8 bits to represent a position, so the maximum
+        # segment is 256.
+        start_ends = subdivide_text(len(self._entity_ids_str), 255, self._max_window)
+        logger.debug(f"Processing text in {len(start_ends)} segment(s)")
 
-        if len(position_results.error_message) > 0:
-            raise Exception(position_results.error_message)
+        for start_idx, end_idx in start_ends:
+            sub_entity_ids = self._entity_ids_str[start_idx:end_idx]
 
-        logger.debug(
-            f"Number of entities with positions to evaluate: {len(position_results.results)}"
-        )
-
-        # Walk through each of the entities and their matching token positions
-        # for the entities that have been seen the required minimum number of
-        # times
-        for entity_result in position_results.results:
-            entity_id = entity_result.entity_id
-            positions = entity_result.pos
-
-            # Get the number of tokens for the entity
-            n_e = self._lookup.num_tokens_for_entity(entity_id)
-            assert n_e is not None
-
-            windows = adds_removes_from_positions(
-                positions, n_e, self._min_window, self._max_window
+            # Call compiled C code to find the entity positions
+            s = "|".join(sub_entity_ids)
+            position_results = calc_positions(
+                s.encode(), self._max_entity_id, min_count
             )
 
-            for start, end, n_adds, n_removes in windows:
+            # Check the positions were calculated successfully
+            if len(position_results.error_message) > 0:
+                raise Exception(position_results.error_message)
 
-                # Calculate the likelihood of the tokens given the entity
-                prob = self._likelihood.calc(n_adds, n_removes, n_e)
+            logger.debug(
+                f"Number of entities with positions to evaluate: {len(position_results.results)}"
+            )
 
-                # If the likelihood is above the threshold, then store the match
-                if prob > self._min_probability:
-                    self._matches.append(
-                        ProbabilisticMatch(
-                            start=start, end=end, entity_id=entity_id, probability=prob
+            # Walk through each of the entities and their matching token positions
+            # for the entities that have been seen the required minimum number of
+            # times
+            for entity_result in position_results.results:
+                entity_id = entity_result.entity_id
+                positions = entity_result.pos
+
+                # Get the number of tokens for the entity
+                n_e = self._lookup.num_tokens_for_entity(entity_id)
+                assert n_e is not None
+
+                windows = adds_removes_from_positions(
+                    positions, n_e, self._min_window, self._max_window
+                )
+
+                for start, end, n_adds, n_removes in windows:
+
+                    # Calculate the likelihood of the tokens given the entity
+                    prob = self._likelihood.calc(n_adds, n_removes, n_e)
+
+                    # If the likelihood is above the threshold, then store the
+                    # match if it hasn't been seen before
+                    if prob > self._min_probability:
+                        m = ProbabilisticMatch(
+                            start=start + start_idx,  # Apply the segment offset
+                            end=end + start_idx,
+                            entity_id=entity_id,
+                            probability=prob,
                         )
-                    )
+                        if m not in self._matches:
+                            self._matches.append(m)
 
         return self._matches
 
@@ -114,3 +154,4 @@ class EntityMatcherAddRemove(EntityMatcher):
 
         self._tokens = []
         self._matches = []
+        self._entity_ids_str = []
--- Python/DataScience/entity-extraction/entity/test_matcher_add_remove.py
+++ Python/DataScience/entity-extraction/entity/test_matcher_add_remove.py
@@ -1,6 +1,11 @@
 from entity.matcher_add_remove import (
+    EntityMatcherAddRemove,
     adds_removes_from_positions,
+    subdivide_text,
 )
+from likelihood.likelihood_add_remove import make_likelihood_add_remove_symmetric
+from lookup.in_memory_lookup import InMemoryLookup
+from text.tokeniser import tokenise_text
 
 
 def test_adds_removes_from_positions():
@@ -31,3 +36,79 @@ def test_adds_removes_from_positions():
         (2, 4, 1, 1),
         (2, 5, 1, 0),
     ]
+
+
+def test_subdivide_test():
+    """Unit tests for subdivide_text()."""
+
+    # Text fits within a single segment
+    assert subdivide_text(8, 10, 5) == [(0, 8)]
+    assert subdivide_text(10, 10, 5) == [(0, 10)]
+
+    # Text spans two segments
+    # Token index:   0 1 2 3 4 5 6 7 8 9 10
+    # Segment 0:     =====
+    # Segment 1:       =====
+    assert subdivide_text(4, 3, 3) == [(0, 3), (1, 4)]
+
+    # Token index:   0 1 2 3 4 5 6 7 8 9 10
+    # Segment 0:     =====
+    # Segment 1:         =====
+    assert subdivide_text(5, 3, 2) == [(0, 3), (2, 5)]
+
+    # Text spans two segments
+    # Token index:   0 1 2 3 4 5 6 7 8 9 10
+    # Segment 0:     =========
+    # Segment 1:           =========
+    # Segment 2:                 =======
+    assert subdivide_text(10, 5, 3) == [(0, 5), (3, 8), (6, 10)]
+
+
+def test_matcher_add_remove():
+
+    # Create an in-memory lookup
+    lookup = InMemoryLookup()
+
+    # Add the entities to the lookup
+    entities = [
+        (0, "100", "78 Straight Street London"),
+        (1, "101", "6 The Walk London"),
+        (2, "102", "10 The Mews Birmingham"),
+        (3, "103", "12 The Mews Birmingham"),
+    ]
+
+    for internal_id, external_id, text in entities:
+        tokens = tokenise_text(text)
+        assert tokens is not None
+        lookup.add(internal_id, external_id, tokens)
+
+    # Make a simple likelihood function
+    likelihood_symmetric = make_likelihood_add_remove_symmetric(0.2, 0.9, 0.5, 0.1)
+
+    # Instantiate the entity matcher
+    matcher = EntityMatcherAddRemove(
+        lookup=lookup,
+        likelihood=likelihood_symmetric,
+        min_window=2,
+        max_window=5,
+        min_probability=0.8,
+        max_entity_id=3,
+    )
+
+    for start_idx in range(240, 260):
+
+        tokens = ["a" for _ in range(start_idx)]
+        tokens.extend(["78", "straight", "street", "london"])
+        tokens.extend(["b" for _ in range(20)])
+
+        for t in tokens:
+            matcher.next_token(t)
+
+        matches = matcher.get_matches()
+
+        assert len(matches) == 1
+        assert matches[0].entity_id == 0
+        assert matches[0].start == start_idx
+        assert matches[0].end == matches[0].start + 3
+
+        matcher.reset()

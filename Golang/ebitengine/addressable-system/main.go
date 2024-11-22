65e7e9ccdfbad5a8b49a38194b4dec30fe2700e9
Chris Claxton <c.d.claxton@gmail.com>
2024-11-22 18:43:40 +0000

    Add background

--- Golang/ebitengine/addressable-system/main.go
+++ Golang/ebitengine/addressable-system/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"embed"
+	"image"
 	"log"
+
+	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type Game struct{
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+)
 
+//go:embed assets/*
+var assets embed.FS
+
+var Background = mustLoadImage("assets/alarm-panel.png")
+
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
+
+type Game struct {
+	background *Sprite
 }
 
 func NewGame() *Game {
 	return &Game{
-
+		background: NewSprite(Background, 0, 0),
 	}
 }
 
@@ -21,16 +50,16 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	g.background.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth int, outsideHeight int) (int, int) {
-	return 320, 240
+	return ScreenWidth, ScreenHeight
 }
 
 func main() {
 	game := NewGame()
-	ebiten.SetWindowSize(320, 240)
+	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("")
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)

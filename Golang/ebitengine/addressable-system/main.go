4ac94813e8e83c737cd4f2039f9e0a1150d56866
Chris Claxton <c.d.claxton@gmail.com>
2024-11-23 08:17:14 +0000

    Add buttons

--- Golang/ebitengine/addressable-system/main.go
+++ Golang/ebitengine/addressable-system/main.go
@@ -20,6 +20,7 @@ var assets embed.FS
 
 var Background = mustLoadImage("assets/alarm-panel.png")
 var LedOn = mustLoadImage("assets/led-on.png")
+var ButtonPressed = mustLoadImage("assets/button-pressed.png")
 
 func mustLoadImage(name string) *ebiten.Image {
 	f, err := assets.Open(name)
@@ -58,11 +59,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	g.background.Draw(screen)
 	g.alarmPanel.fireLed.Draw(screen)
 	g.alarmPanel.powerFailureLed.Draw(screen)
+
 	g.alarmPanel.fireZone1Led.Draw(screen)
 	g.alarmPanel.fireZone2Led.Draw(screen)
 	g.alarmPanel.fireZone3Led.Draw(screen)
 	g.alarmPanel.fireZone4Led.Draw(screen)
 	g.alarmPanel.fireZone5Led.Draw(screen)
+
+	g.alarmPanel.lampTestButton.Draw(screen)
+	g.alarmPanel.soundAlarmButton.Draw(screen)
+	g.alarmPanel.stopAlarmButton.Draw(screen)
+	g.alarmPanel.exitButton.Draw(screen)
+	g.alarmPanel.enterButton.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth int, outsideHeight int) (int, int) {

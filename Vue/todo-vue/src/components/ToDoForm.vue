bb9a18dd424ef9ef9e3256980cc4774a4822ed71
Chris Claxton <c.d.claxton@gmail.com>
2024-12-07 08:40:49 +0000

    Todo app using Vue and JS

---
+++ Vue/todo-vue/src/components/ToDoForm.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.label === '') {
+        return
+      }
+      this.$emit('todo-added', this.label)
+      this.label = ''
+    },
+  },
+  data() {
+    return {
+      label: '',
+    }
+  },
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label_lg">What needs to be done?</label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.trim.lazy="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">Add</button>
+  </form>
+</template>

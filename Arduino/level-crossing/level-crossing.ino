c02e9de41d8435e4fe7199e1142f3397e091adc5
cdclaxton <cdclaxton@gmail.com>
2025-11-23 13:02:05 +0000

    Arduino-based level crossing

---
+++ Arduino/level-crossing/level-crossing.ino
@@ -0,0 +1,254 @@
+// Buttons
+#define LEFT_BARRIER_UP A0
+#define LEFT_BARRIER_DOWN A1
+#define RIGHT_BARRIER_UP A2
+#define RIGHT_BARRIER_DOWN A3
+#define YELLOW_BUTTON A4
+#define RED_BUTTON A5
+#define BLACK_BUTTON 2
+
+// Buzzer
+#define BUZZER 3
+
+// LEDs
+#define RED_LED_1 4
+#define RED_LED_2 5
+#define YELLOW_LED 6
+
+// Motor 1
+#define MOTOR1_ENABLE 8
+#define MOTOR1_IP1 9
+#define MOTOR1_IP2 10
+
+// Motor 2
+#define MOTOR2_ENABLE 11
+#define MOTOR2_IP1 12
+#define MOTOR2_IP2 13
+
+// States of the buttons
+bool redLightsFlashing = LOW;
+bool redLightsLit = LOW;
+
+bool yellowLightFlashing = LOW;
+bool yellowLightLit = LOW;
+
+bool blackState = LOW;
+
+// Minimum time between button presses to debounce
+#define debounceButtonTime 1000
+
+// On and off times for the yellow light
+#define YELLOW_OFF_TIME 500
+#define YELLOW_ON_TIME 500
+unsigned long lastYellowStateChange = millis();
+bool yellowFlashState = HIGH;
+
+// On and off times for the red light
+#define RED_ON_TIME 500
+unsigned long lastRedStateChange = millis();
+bool redFlashLeftState = HIGH;
+
+// Buzzer
+#define BUZZER_TIME 500
+#define BUZZER_FREQ_1 500
+#define BUZZER_FREQ_2 800
+unsigned long lastBuzzerStateChange = millis();
+bool buzzerState1 = HIGH;
+
+// Last time each button was pressed
+unsigned long lastRedPress = millis();
+unsigned long lastYellowPress = millis();
+unsigned long lastBlackPress = millis();
+
+// Barrier on and off cycles
+#define BARRIER_ON_CYCLES 3
+#define BARRIER_OFF_CYCLES 5
+int numCyclesSinceLastMotor1Move = 0;
+int numCyclesSinceLastMotor2Move = 0;
+
+void setup() {
+  // Inputs
+  pinMode(BLACK_BUTTON, INPUT);
+  pinMode(BUZZER, INPUT);
+
+  // Outputs
+  pinMode(RED_LED_1, OUTPUT);
+  pinMode(RED_LED_2, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+
+  pinMode(MOTOR1_ENABLE, OUTPUT);
+  pinMode(MOTOR1_IP1, OUTPUT);
+  pinMode(MOTOR1_IP2, OUTPUT);
+
+  pinMode(MOTOR2_ENABLE, OUTPUT);
+  pinMode(MOTOR2_IP1, OUTPUT);
+  pinMode(MOTOR2_IP2, OUTPUT);
+}
+
+void raiseBarrier1() {
+  digitalWrite(MOTOR1_IP1, HIGH);
+  digitalWrite(MOTOR1_IP2, LOW);
+}
+
+void lowerBarrier1() {
+  digitalWrite(MOTOR1_IP1, LOW);
+  digitalWrite(MOTOR1_IP2, HIGH);
+}
+
+void holdBarrier1() {
+  digitalWrite(MOTOR1_IP1, LOW);
+  digitalWrite(MOTOR1_IP2, LOW);  
+}
+
+void raiseBarrier2() {
+  digitalWrite(MOTOR2_IP1, HIGH);
+  digitalWrite(MOTOR2_IP2, LOW);
+}
+
+void lowerBarrier2() {
+  digitalWrite(MOTOR2_IP1, LOW);
+  digitalWrite(MOTOR2_IP2, HIGH);
+}
+
+void holdBarrier2() {
+  digitalWrite(MOTOR2_IP1, LOW);
+  digitalWrite(MOTOR2_IP2, LOW);  
+}
+
+void loop() {
+
+  // Current time in milliseconds
+  unsigned long currentTime = millis();
+
+  // ----------------------------------------------------------------------------------------------
+  // Yellow lights
+  // ----------------------------------------------------------------------------------------------
+
+  if ((digitalRead(YELLOW_BUTTON) == HIGH) && (currentTime > (lastYellowPress + debounceButtonTime))) {
+    yellowLightFlashing = !yellowLightFlashing;
+    lastYellowPress = currentTime;
+
+    if (yellowLightFlashing == HIGH) {
+      redLightsFlashing = LOW;
+    }
+  }  
+
+  if (yellowLightFlashing == HIGH) {
+    if (((yellowFlashState == HIGH) && (currentTime > (lastYellowStateChange + YELLOW_ON_TIME))) ||
+      ((yellowFlashState == LOW) && (currentTime > (lastYellowStateChange + YELLOW_OFF_TIME)))) {
+
+      yellowLightLit = !yellowLightLit;
+      yellowFlashState = !yellowFlashState;
+      lastYellowStateChange = currentTime;
+    }
+  } else {
+    yellowLightLit = LOW;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Red lights
+  // ----------------------------------------------------------------------------------------------
+
+  if ((digitalRead(RED_BUTTON) == HIGH) && (currentTime > (lastRedPress + debounceButtonTime))) {
+    redLightsFlashing = !redLightsFlashing;
+    lastRedPress = currentTime;
+
+    if (redLightsFlashing == HIGH) {
+      yellowLightFlashing = LOW;
+      redFlashLeftState = LOW;
+    }
+  }
+
+  if ((redLightsFlashing == HIGH) && (currentTime > lastRedStateChange + RED_ON_TIME)) {
+      redFlashLeftState = !redFlashLeftState;
+      lastRedStateChange = currentTime;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Buzzer
+  // ---------------------------------------------------------------------------------------------- 
+
+  if ((digitalRead(BLACK_BUTTON) == HIGH) && (currentTime > (lastBlackPress + debounceButtonTime))) {
+    blackState = !blackState;
+    lastBlackPress = currentTime;
+
+    if (blackState == HIGH) {
+      buzzerState1 = HIGH;
+    }
+  }
+
+  if ((blackState == HIGH) && (currentTime > lastBuzzerStateChange + BUZZER_TIME)) {
+    buzzerState1 = !buzzerState1;
+    lastBuzzerStateChange = currentTime;
+    if (buzzerState1 == HIGH) {
+      tone(BUZZER, BUZZER_FREQ_1, BUZZER_TIME);
+    } else {
+      tone(BUZZER, BUZZER_FREQ_2, BUZZER_TIME);
+    }
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Set lights
+  // ---------------------------------------------------------------------------------------------- 
+
+  digitalWrite(YELLOW_LED, yellowLightLit);
+  digitalWrite(RED_LED_1, (redLightsFlashing == HIGH) && (redFlashLeftState == HIGH));
+  digitalWrite(RED_LED_2, (redLightsFlashing == HIGH) && (redFlashLeftState == LOW));
+
+  // ----------------------------------------------------------------------------------------------
+  // Motor 1 for the left barrier
+  // ----------------------------------------------------------------------------------------------   
+
+  if (((digitalRead(LEFT_BARRIER_UP) == HIGH) && (digitalRead(LEFT_BARRIER_DOWN) == LOW)) ||
+    ((digitalRead(LEFT_BARRIER_UP) == LOW) && (digitalRead(LEFT_BARRIER_DOWN) == HIGH))) {
+
+      digitalWrite(MOTOR1_ENABLE, HIGH);
+
+      if (numCyclesSinceLastMotor1Move < BARRIER_ON_CYCLES) {
+        if (digitalRead(LEFT_BARRIER_UP) == HIGH) {
+          raiseBarrier1();
+        } else {
+          lowerBarrier1();
+        }
+      } else {
+        holdBarrier1();
+      }
+
+      if (numCyclesSinceLastMotor1Move == (BARRIER_ON_CYCLES + BARRIER_OFF_CYCLES)) {
+        numCyclesSinceLastMotor1Move = 0;
+      } else {
+        numCyclesSinceLastMotor1Move = numCyclesSinceLastMotor1Move + 1;
+      }      
+  } else {
+    digitalWrite(MOTOR1_ENABLE, LOW);
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Motor 2 for the right barrier
+  // ----------------------------------------------------------------------------------------------   
+
+  if (((digitalRead(RIGHT_BARRIER_UP) == HIGH) && (digitalRead(RIGHT_BARRIER_DOWN) == LOW)) ||
+    ((digitalRead(RIGHT_BARRIER_UP) == LOW) && (digitalRead(RIGHT_BARRIER_DOWN) == HIGH))) {
+
+      digitalWrite(MOTOR2_ENABLE, HIGH);
+      
+      if (numCyclesSinceLastMotor2Move < BARRIER_ON_CYCLES) {
+        if (digitalRead(RIGHT_BARRIER_UP) == HIGH) {
+          raiseBarrier2();
+        } else {
+          lowerBarrier2();
+        }
+      } else {
+        holdBarrier2();
+      }
+
+      if (numCyclesSinceLastMotor2Move == (BARRIER_ON_CYCLES + BARRIER_OFF_CYCLES)) {
+        numCyclesSinceLastMotor2Move = 0;
+      } else {
+        numCyclesSinceLastMotor2Move = numCyclesSinceLastMotor2Move + 1;
+      }      
+  } else {
+    digitalWrite(MOTOR2_ENABLE, LOW);
+  }
+
+}
---
+++ Arduino/level-crossing/readme.md
@@ -0,0 +1,11 @@
+# Level crossing
+
+A children's level crossing toy with:
+
+* Two sets of flashing lights;
+* Buzzer; and
+* Independenty controllable barrier motors.
+
+## Schematic
+
+![Schematic](./schematic.png)
